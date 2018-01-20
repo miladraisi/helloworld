6e02a15d3be11b04ccf04e7505c79cb4e08af5d2
Milad Raisi <miladraisi@gmail.com>
2018-01-21 03:05:20 +0330

    commit 20

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 def main():
-    print('--Hello World!--')
-    for i in range(1, 10):
-        for j in range(1, 10):
-            print(format(i*j, '3d'), end = '')
-        print()
+    print('---Hello World!---')
 
 if __name__ == "__main__": main()
